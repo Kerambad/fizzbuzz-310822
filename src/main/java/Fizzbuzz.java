f9ddb0c369fdd151ce38aab9cbd8d0c76bbfa00a
marek <marek-schuetze@mail.de>
2022-08-31 16:59:27 +0200

    Update fizzbuzz method

--- src/main/java/Fizzbuzz.java
+++ src/main/java/Fizzbuzz.java
@@ -1,10 +1,29 @@
 public class Fizzbuzz {
     public static void main(String[] args) {
 
+        int num = 5;
+        String solution = fizzbuzz(num);
+        print(solution);
+
     }
     public static String fizzbuzz(int number) {
 
+        if ((number % 3) == 0 && (number % 5) == 0) {
+            return "fizzbuzz";
+        }
+        else if ((number % 5) == 0) {
+            return "buzz";
+        }
+        else if ((number % 3) == 0) {
+            return "fizz";
+        }
+        else {
+            String s = String.valueOf(number);
+            return s;
+        }
+    }
 
-        return "fizz";
+    public static void print(String input){
+        System.out.println(input);
     }
 }
