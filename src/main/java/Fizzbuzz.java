dadbffaef231218dcf71936f1cbf3865f756d43d
marek <marek-schuetze@mail.de>
2022-08-31 18:26:12 +0200

    Updates:
    +input method
    +again method;
    +small changes

--- src/main/java/Fizzbuzz.java
+++ src/main/java/Fizzbuzz.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Fizzbuzz {
     public static void main(String[] args) {
 
-        int num = 5;
-        String solution = fizzbuzz(num);
+
+        String solution = fizzbuzz(input());
+
         print(solution);
+
+        again();
 
     }
     public static String fizzbuzz(int number) {
@@ -18,12 +23,29 @@ public class Fizzbuzz {
             return "fizz";
         }
         else {
-            String s = String.valueOf(number);
-            return s;
+
+            return String.valueOf(number);
         }
     }
 
     public static void print(String input){
         System.out.println(input);
+    }
+
+    public static int input() {
+        System.out.println("Please enter a number:");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        return num;
+    }
+    public static void again(){
+
+        System.out.println("Run again? if yes enter y, for no type any key");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        if (input.equals("y")) {
+            main(new String[]{"main" });
+        }
     }
 }
